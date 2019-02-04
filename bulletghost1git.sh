a7b43d09e372e62b745580d2af73fdcd8b9c6dd6
bulletghost1 <20.juan.barraza@ksd.org>
2019-02-04 12:18:27 -0800

    AA

---
+++ bulletghost1git.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  bulletghost1;
+git config --global user.name  bulletghost1;
+git config --global user.user bulletghost1;
+git config --global user.email 20.juan.barraza@ksd.org;
+git config --list;
